9813d8aa555226ee38f20f6014156427bc5d6ce5
gMinos <gjosebelarmino@gmail.com>
2024-06-08 18:38:40 -0500

    creando el modelo de continente

--- applications/continente/models.py
+++ applications/continente/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+# from applications.pais.models import Pais
 
-# Create your models here.
+
+class Continente(models.Model):
+    nombre_continente = models.CharField(
+        'Nombre continente', max_length=20, unique=True)
+    descripcion_continente = models.CharField(
+        'Descripcion continente', max_length=50, unique=True)
+    # continente_pais = models.ManyToManyField(Pais)
+
+    def __str_(self):
+        return f'{self.id} - {self.nombre_continente} - {self.descripcion_continente}'
