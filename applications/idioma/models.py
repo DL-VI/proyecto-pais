ef8b2cd5811f705236ea73a890566efc95ad4cb7
gMinos <gjosebelarmino@gmail.com>
2024-06-08 19:10:52 -0500

    se mejorado la sintaxis del idioma/models.py

--- applications/idioma/models.py
+++ applications/idioma/models.py
@@ -1,7 +1,10 @@
 from django.db import models
-#from applications.pais.models import Pais
-# Create your models here.
+# from applications.pais.models import Pais
+
+
 class Idioma(models.Model):
-    codigoIdioma = models.CharField('Codigo Idioma', max_length= 10, unique=True)
-    nombreIdioma = models.CharField('Nombre Idioma', max_length=15, unique=True)
-    #idioma_pais = models.ForeignKey(Pais, on_delete= models.CASCADE)
+    codigoIdioma = models.CharField(
+        'Codigo Idioma', max_length=10, unique=True)
+    nombreIdioma = models.CharField(
+        'Nombre Idioma', max_length=15, unique=True)
+    # idioma_pais = models.ForeignKey(Pais, on_delete= models.CASCADE)
