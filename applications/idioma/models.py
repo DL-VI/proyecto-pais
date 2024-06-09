8a9545dfac176b2f593b8d29d6955e468b90c550
Natanael <reyesnatanael28@gmail.com>
2024-06-08 19:03:02 -0500

    tabla idioma

--- applications/idioma/models.py
+++ applications/idioma/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+#from applications.pais.models import Pais
 # Create your models here.
+class Idioma(models.Model):
+    codigoIdioma = models.CharField('Codigo Idioma', max_length= 10, unique=True)
+    nombreIdioma = models.CharField('Nombre Idioma', max_length=15, unique=True)
+    #idioma_pais = models.ForeignKey(Pais, on_delete= models.CASCADE)
