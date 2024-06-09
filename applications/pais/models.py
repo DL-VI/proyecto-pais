6339fcc0899303bdb9309bfdebf3153b664391ab
Fabiano Del Villar <fabianodelvillar@gmail.com>
2024-06-08 19:19:26 -0500

    tabla Pais

--- applications/pais/models.py
+++ applications/pais/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Pais(models.Model):
+    codigoPais = models.CharField('Codigo Pais', max_length=10, unique=True)
+    nombrePais = models.CharField('Nombre Pais', max_length=30, unique=True)
+
+    def __str__(self) -> str:
+        return self.codigoPais + '-' + self.nombrePais
