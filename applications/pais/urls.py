22dd422445304ce2af6faaa4b4bfbe3e4e65c093
gMinos <gjosebelarmino@gmail.com>
2024-06-12 20:32:37 -0500

    simplemente metiendo carpinteria
    - se ha cambiado el nombre de la clase del archivo pais/views.py para que quede claro que la clase sera para mostrar los idiomas de los paises
    - se cambio el llamado desde pais/urls.py ya que la clase en views fue cambiada
    - ahora se renderiza pais.html, que este muestra los paises con sus idiomas

--- applications/pais/urls.py
+++ applications/pais/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('paises/', views.TodosPaises.as_view()),
-    path('paises-ciudad/', views.TodosPaises.as_view()),
+    path('paises/', views.idomasPaises.as_view()),
+    path('paises-ciudad/', views.idomasPaises.as_view()),
 ]
--- applications/pais/views.py
+++ applications/pais/views.py
@@ -2,7 +2,7 @@ from django.views.generic import ListView
 from .models import Pais
 
 
-class TodosPaises(ListView):
-    template_name = 'pais/pais-ciudad.html'
+class idomasPaises(ListView):
+    template_name = 'pais/pais.html'
     context_object_name = 'paises'
     model = Pais
